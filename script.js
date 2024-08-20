66a62720ae4c4dc27b18bbfc0b2bae22e1a819a4
Igao2 <igor.dasilvacosta8@gmail.com>
2024-08-20 07:56:54 -0300

    consumindo a api de datas

--- script.js
+++ script.js
@@ -8,15 +8,34 @@ function calc()
     
     
     var diferenca = agora - dataAlvo;
-    
-    var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-    var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
     var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
-
-    var exibr = dias + " dias, "+ horas + ":"+ minutos+":"+ segundos + " horas, sem o João fazer o pix"
-    console.log(exibr)
-    horario.innerHTML = exibr
+    var dia = agora.getDate();
+    var mes = agora.getMonth();
+    var ano = agora.getFullYear();
+    var hora = agora.getHours();
+    var minuto = agora.getMinutes();
+    
+     const formattedDate = `${dia}/${mes}/${ano}`;
+     const formattedTime = `${hora}:${minuto}`;
+    
+   
+    
+    //"
+  fetch(`https://date-api-sikn.onrender.com/diferencadatas?datai=15/10/2023&h1=18:10&dataf=${formattedDate}&h2=${formattedTime}&format=YMdhm`) // Substitua "/endpoint" pelo seu endpoint específico
+  .then(response => response.json())
+  .then(data =>{
+    const result = data.result;
+    const hours = result.hours;
+    const days =result.days;
+    const years = result.years;
+    const months = result.months;
+    const minutes = result.minutes;
+    var exibr =years+" anos, "+months+" meses, "+days+ " dias, "+ hours + ":"+ minutes+ ":"+segundos+" horas, sem o João fazer o pix"
+    horario.innerHTML = exibr;
+    
+})
+  .catch(error => console.error('Erro:', error));
+   
 
 }
 setInterval(calc,1000)
